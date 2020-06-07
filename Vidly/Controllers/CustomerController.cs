9c4ed6933b441f9973a193f5110d8ccfbff55d32
jaco <jaco.kleynhans@icloud.com>
2020-06-07 20:25:38 +0200

    First Commit with Identity Framework

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public List<Customer> Customers { get; set; } = new List<Customer>
+        {
+            new Customer {Id = 1, Name = "John Smith"},
+            new Customer {Id = 2, Name = "Mary Williams"}
+        };
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(Customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return Content(Customers.FirstOrDefault(p => p.Id == id)?.Name);
+        }
+    }
+}
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name = "Customer 1"},
+                new Customer{Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel { Movie = movie, Customers = customers };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id =" + id);
+        }
+
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie{Id = 0, Name = "Shrek"},
+                new Movie{Id = 1, Name = "Wall-e"}
+            };
+            return View(movies);
+        }
+    }
+}
